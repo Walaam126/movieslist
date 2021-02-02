9591bb13fec64dc500ca3d596ffcccccc84b2419
IsmailQamber <ism-you-95@hotmail.com>
2021-02-02 15:09:14 +0300

    data added

---
+++ src/data.js
@@ -0,0 +1,17 @@
+const WatchList = [
+  {
+    id: 1,
+    name: "into the wild",
+    watch: "watched",
+  },
+  {
+    id: 2,
+    name: "Enemy",
+    watch: "unwatch",
+  },
+  {
+    id: 3,
+    name: "Harry potter",
+    watch: "watched",
+  },
+];
